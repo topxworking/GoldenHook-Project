5d2391ba2d320cb694ddefc20ad66c875cdba374
TOPX <topx.working@gmail.com>
2026-02-23 16:50:06 +0700

    add seazone

---
+++ Golden Hook/Assets/Projects/Scripts/Data/SeaZoneData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class FishSpawnEntry
+{
+    public FishData fishData;
+    [Range(0f, 1f)]
+    public float spawnWeightOveride = 1f;
+}
+
+[CreateAssetMenu(fileName = "SeaZoneData", menuName = "FishingGame/SeaZoneData")]
+public class SeaZoneData : ScriptableObject
+{
+    [Header("Identity")]
+    public string zoneName;
+    public Sprite zoneBackground;
+    public Color zoneThemeColor = Color.blue;
+    public int ZoneIndex = 0;
+
+    [Header("Fish Pool")]
+    public List<FishSpawnEntry> fishPool = new();
+
+    [Header("Unlock")]
+    public bool isUnlocked = false;
+    public int unlockCost = 0;
+    public int reqiredZoneIndex = -1;
+
+    [Header("Zone Modifiers")]
+    public float rareFishBonus = 0f;
+    public float incomeMultipier = 1f;
+}
